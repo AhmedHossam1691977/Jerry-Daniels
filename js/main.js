e9ca5720b104b2da83a9ee3bbda2e957f8724a99
AhmedHossam1691977 <ahmedhossam1691977@gmail.comg>
2023-11-13 01:21:39 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 600) {
+        $('.navbar').css('background-color','#00000067')
+    } else {
+        $('.navbar').css()
+    }
+});
